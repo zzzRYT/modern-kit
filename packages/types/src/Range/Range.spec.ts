3d6fdcf0ac70cb146502e0844fff8ee22f67433b
zzzRYT <jinjinstar3@gmail.com>
2025-06-07 16:41:59 +0900

    feat(types): Range type 주석 추가

--- packages/types/src/Range/Range.spec.ts
+++ packages/types/src/Range/Range.spec.ts
@@ -2,21 +2,17 @@ import { describe, it, expectTypeOf } from 'vitest';
 import { Range } from '.';
 
 describe('Range', () => {
-  it('범위 내의 숫자라면 타입을 반환합니다.', () => {
+  it('범위 내 숫자라면 정상 출력하고, 범위 밖 숫자라면 타입에러를 발생시킵니다.', () => {
     const intraRangeIntegers: Range<1, 5> = 3;
+    const outOfRangeInteger = 6 as unknown as Range<1, 5>;
 
     expectTypeOf(intraRangeIntegers).toEqualTypeOf<3>();
+    expectTypeOf(outOfRangeInteger).not.toMatchTypeOf<6>();
   });
 
-  it('범위 밖의 숫자라면 타입 에러를 발생시킵니다.', () => {
-    // @ts-expect-error
-    const outOfRange: Range<1, 5> = 7;
-    expectTypeOf(outOfRange).not.toEqualTypeOf<7>();
-  });
+  it('<number, number>타입 이라면 해당 타입을 반환하고, <number, number>타입이 아니라면 never를 반환합니다.', () => {
+    const otherTypeRange: Range<1, '5'> = null as unknown as Range<1, '5'>;
 
-  it('Range 타입이 정수가 아니라면 에러를 발생시킵니다.', () => {
-    // @ts-expect-error
-    const wrong: Range<1, 5> = 'hello world';
-    expectTypeOf(wrong).not.toEqualTypeOf<'hello world'>();
+    expectTypeOf(otherTypeRange).toEqualTypeOf<never>();
   });
 });
